1a4bade59edc62acefaf1b37e0602d31bcac6b25
Sebastian Pfeufer <sebastianpfeufer@icloud.com>
2022-02-09 12:04:49 +0100

    Created components

---
+++ src/components/FlatList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FlatList = () => {
+  return (
+    <p>This will be the flatlist</p>
+  );
+};
+
+export default FlatList;
---
+++ src/components/Marker.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Marker = () => {
+  return (
+    <p>This will be the marker</p>
+  );
+};
+
+export default Marker;
