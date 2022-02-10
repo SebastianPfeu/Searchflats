99d34bb054a384d40c261f7ad63d7db037533781
Sebastian Pfeufer <sebastianpfeufer@icloud.com>
2022-02-10 19:27:19 +0100

    Added logic to FlatList component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,30 @@
 /* eslint-disable */
 import React, { Component } from 'react';
+import flats from '../../data/flats';
 import FlatList from './FlatList';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state={
+      selectedFlat: flats[0],
+      flats
+    }
+  }
+
+  selectFlat = (index) => {
+    this.setState({ selectedFlat: flats[index] })
+  }
+
   render() {
     return (
       <div>
-        <FlatList>
-
-        </FlatList>
+        <FlatList
+          flats={this.state.flats}
+          selectedFlat={this.state.selectedFlat}
+          selectFlat={this.selectFlat}
+        />
         <div>
 
         </div>
